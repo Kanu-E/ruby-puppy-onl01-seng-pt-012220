1771f10463fc5e25448683c4de0570b5f9c237e9
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-09 03:19:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,12 +10,12 @@ class Dog
   end
 
   def self.all
-    @@all.each do |dog|
-        end
+      @@all.each do |dog|
+      puts dog.name
+    end
   end
 
   def self.clear_all
     @@all.clear
   end
-  
 end
